a3c43f7c9d881ce9a13ff9f5c3b9018125124c50
MysticAlpaca <shyguy50507@gmail.com>
2016-11-09 14:46:21 -0800

    Add files via upload

---
+++ rgwetgrgh.py
@@ -0,0 +1,34 @@
+print('what is your favorite undertale character?')
+answer = raw_input()
+if answer == "Sans":
+        print("yayy")
+else:
+        print("try again")
+
+
+print('do you like pokemons')
+answer = raw_input()
+if answer == "yes":
+        print("POkEMoNS")
+else:
+        print("NOOOOO")
+
+print('DO YUO LIKE PIKACHU')
+answer = raw_input()
+if answer == "yes":
+        print("why?")
+else:
+        print("ok")
+
+print("do you like bottle flips")
+answer = raw_input()
+if answer =="yes":
+        print("RAGING KID")
+else:
+        ("WHY")
+        print("WHYYYYY")
+
+        
+
+
+
